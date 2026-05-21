33ba61e5a17bc8c812ecd3172b09723b5e063f79
Afros Rajabov <a.radzhabov@estesis.tech>
2026-05-21 11:54:22 +0700

    pytest

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -56,6 +56,7 @@ if status is-interactive
     abbr -a -- pui 'pip uninstall'
     abbr -a -- pfr 'pip freeze > requirements.txt'
     abbr -a -- pr 'python run.py'
+    abbr -a -- pt pytest
     abbr -a -- pav 'source ./.venv/bin/activate.fish'
 
     # GO
