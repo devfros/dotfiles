c41c92ef07cb9443842d79d48be26a9f5716c716
Afros Rajabov <afrosrajabov1207@mail.ru>
2026-05-29 02:01:31 +0700

    dcu

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -67,6 +67,7 @@ if status is-interactive
     abbr -a -- dstop 'bash stop_all.sh'
     abbr -a -- dps 'docker ps -a'
     abbr -a -- dc 'docker compose'
+    abbr -a -- dcu 'docker compose up -d'
     abbr -a -- dcl 'docker compose -f docker-compose.local.yaml'
     abbr -a -- dcld 'docker compose -f ./deployment/docker-compose.local.yaml'
 
